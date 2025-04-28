8c288bd2f55efe071bc9958f6eb4ed9ea0c627ee
Abdullah <abdullahbmyt@gmail.com>
2025-04-28 14:17:55 +0200

    Prepare project for deployment

--- Abdullah-Store/src/settings.py
+++ Abdullah-Store/src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#4pxqkx2^3*!!if@4j1r*^2v*a-$-4fu4tm2u3an_pg)1_xv9^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -114,7 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
